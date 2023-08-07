1da2002d1db72fb61b383ca3a3460b9e46d912ba
Domae-back-end <ty_ty123@naver.com>
2023-08-07 20:35:50 +0900

    create : 프로젝트 생성

---
+++ src/main/java/com/database/databaseperformance/DatabasePerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.database.databaseperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasePerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasePerformanceApplication.class, args);
+	}
+
+}
